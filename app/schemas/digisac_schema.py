07f8f2b81e59372b93ff456b4adcf0fa17e87da1
davidmrtns <daviddmrtns@gmail.com>
2025-02-18 13:27:42 -0300

    Alteração do schema do Digisac

--- app/schemas/digisac_schema.py
+++ app/schemas/digisac_schema.py
@@ -4,43 +4,43 @@ from datetime import datetime
 
 
 class MessageData(BaseModel):
-    ack: int
-    isNew: bool
-    isFirst: bool
+    ack: Optional[int] = None
+    isNew: Optional[bool] = None
+    isFirst: Optional[bool] = None
 
 class Message(BaseModel):
-    id: str
-    isFromMe: bool
-    sent: bool
-    type: str
-    timestamp: datetime
-    data: MessageData
-    visible: bool
-    accountId: str
-    contactId: str
-    fromId: Optional[str]
-    serviceId: str
-    toId: Optional[str]
-    userId: Optional[str]
-    ticketId: str
-    ticketUserId: Optional[str]
-    ticketDepartmentId: Optional[str]
-    quotedMessageId: Optional[str]
-    origin: Optional[str]
-    createdAt: datetime
-    updatedAt: datetime
-    deletedAt: Optional[datetime]
-    hsmId: Optional[str]
-    isComment: bool
-    reactionParentMessageId: Optional[str]
-    isTranscribing: Optional[bool]
-    transcribeError: Optional[str]
+    id: Optional[str] = None
+    isFromMe: Optional[bool] = None
+    sent: Optional[bool] = None
+    type: Optional[str] = None
+    timestamp: Optional[datetime] = None
+    data: Optional[MessageData] = None
+    visible: Optional[bool] = None
+    accountId: Optional[str] = None
+    contactId: Optional[str] = None
+    fromId: Optional[str] = None
+    serviceId: Optional[str] = None
+    toId: Optional[str] = None
+    userId: Optional[str] = None
+    ticketId: Optional[str] = None
+    ticketUserId: Optional[str] = None
+    ticketDepartmentId: Optional[str] = None
+    quotedMessageId: Optional[str] = None
+    origin: Optional[str] = None
+    createdAt: Optional[datetime] = None
+    updatedAt: Optional[datetime] = None
+    deletedAt: Optional[datetime] = None
+    hsmId: Optional[str] = None
+    isComment: Optional[bool] = None
+    reactionParentMessageId: Optional[str] = None
+    isTranscribing: Optional[bool] = None
+    transcribeError: Optional[str] = None
     text: Optional[str] = None
-    obfuscated: bool
-    file: Optional[Union[str, dict]]
-    files: Optional[Union[List[str], List[dict]]]
-    quotedMessage: Optional[dict]
-    isFromBot: bool
+    obfuscated: Optional[bool] = None
+    file: Optional[Union[str, dict]] = None
+    files: Optional[Union[List[str], List[dict]]] = None
+    quotedMessage: Optional[dict] = None
+    isFromBot: Optional[bool] = None
 
 class Data(BaseModel):
     id: str
